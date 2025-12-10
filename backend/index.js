032d00036641fe9a007e53dbad70b7b450c571b0
siham binta habib <sihambintahabib@gmail.com>
2025-12-11 01:38:20 +0600

    useMutation

--- backend/index.js
+++ backend/index.js
@@ -1,73 +1,93 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const admin = require('firebase-admin')
-const port = process.env.PORT || 3000
-const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
-  'utf-8'
-)
-const serviceAccount = JSON.parse(decoded)
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const admin = require("firebase-admin");
+const port = process.env.PORT || 3000;
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf-8"
+);
+const serviceAccount = JSON.parse(decoded);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-})
+});
 
-const app = express()
+const app = express();
 // middleware
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://b12-m11-session.web.app',
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://b12-m11-session.web.app",
     ],
     credentials: true,
     optionSuccessStatus: 200,
   })
-)
-app.use(express.json())
+);
+app.use(express.json());
 
 // jwt middlewares
 const verifyJWT = async (req, res, next) => {
-  const token = req?.headers?.authorization?.split(' ')[1]
-  console.log(token)
-  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+  const token = req?.headers?.authorization?.split(" ")[1];
+  console.log(token);
+  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
   try {
-    const decoded = await admin.auth().verifyIdToken(token)
-    req.tokenEmail = decoded.email
-    console.log(decoded)
-    next()
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    console.log(decoded);
+    next();
   } catch (err) {
-    console.log(err)
-    return res.status(401).send({ message: 'Unauthorized Access!', err })
+    console.log(err);
+    return res.status(401).send({ message: "Unauthorized Access!", err });
   }
-}
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
+const url = process.env.MONGODB_URI;
+
+// const url = `mongodb+srv://assairment11:ulvO0P03K1Jeo63I@cluster0.hohlyvu.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(url, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 async function run() {
   try {
+    //db collection: clubsCollection
+    const db = client.db("clubsdb");
+    const clubsCollection = db.collection("clubs");
+    //save add-club to db
+    app.post("/clubs", async (req, res) => {
+      const clubData = req.body; //plantsdata =1.5
+      console.log(clubData);
+      const result = await clubsCollection.insertOne(clubData);
+      res.send(result);
+    });
+    // get all club from db
+    app.get("/clubs", async (req, res) => {
+      const result = await clubsCollection.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from Server..')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from Server..assairment 11");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- frontend/src/components/Form/AddPlantForm.jsx
+++ frontend/src/components/Form/AddPlantForm.jsx
@@ -1,100 +1,264 @@
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { imageUpload } from "../../utils";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import LoadingSpinner from "../Shared/LoadingSpinner";
+import ErrorPage from "../../pages/ErrorPage";
+import { TbFidgetSpinner } from "react-icons/tb";
+
 const AddPlantForm = () => {
+  const { user } = useAuth();
+  const queryClient = useQueryClient();
+  // useMutation hook case :
+  // Mutations
+  const {
+    isPending,
+    isError,
+    mutateAsync,
+    reset: mutatereset,
+  } = useMutation({
+    mutationFn: async (cdata) => {
+      return await axios.post(`${import.meta.env.VITE_API_URL}/clubs`, cdata);
+    },
+    onMutate: (cdata) => {
+      console.log("I will post this data--->", cdata);
+    },
+    onSuccess: (data) => {
+      console.log("onsuccess", data);
+      //ADD TOAST
+      toast.success("add club Successful");
+      //reset form after post completed
+      mutatereset();
+      //navigate to page
+
+      // Invalidate and refetch
+      // queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (errors) => {
+      console.log(errors);
+    },
+
+    onSettled: (data, error) => {
+      if (data) console.log(data);
+      if (error) console.log(error);
+    },
+    retry: true,
+
+    //
+  });
+  //react hook :
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const { name, price, location, image, description, category } = data;
+
+    try {
+      //* img :
+      const imgfile = image[0];
+      const imgURL = await imageUpload(imgfile);
+      console.log(imgURL);
+      console.log(data);
+      //* add club data to db
+      const clubData = {
+        clubName: name,
+        description,
+        category,
+        location,
+        membershipFee: parseInt(price),
+        status: "pending",
+        // quantity,
+        managerEmail: user.email,
+        createAt: new Date(),
+        updateAt: new Date(),
+        bannerImage: imgURL,
+      };
+      // const { data } = await axios.post(
+      //   `${import.meta.env.VITE_API_URL}/clubs`,
+      //   clubData
+      // );
+      // // console.log(result);
+      console.log(data);
+      console.table(clubData);
+      await mutateAsync(clubData);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      // toast.success("add club Successful");
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.message);
+    }
+  };
+  if (isPending) return <LoadingSpinner></LoadingSpinner>;
+  if (isError) return <ErrorPage></ErrorPage>;
+
   return (
-    <div className='w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50'>
-      <form>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
-          <div className='space-y-6'>
+    <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
+      <title> add club (manager ) </title>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+          <div className="space-y-6">
             {/* Name */}
-            <div className='space-y-1 text-sm'>
-              <label htmlFor='name' className='block text-gray-600'>
+            <div className="space-y-1 text-sm">
+              <label htmlFor="name" className="block text-gray-600">
                 Name
               </label>
               <input
-                className='w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
-                name='name'
-                id='name'
-                type='text'
-                placeholder='Plant Name'
-                required
+                className="w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                id="name"
+                type="text"
+                placeholder="Club Name"
+                {...register("name", { required: "name is required" })}
               />
-            </div>
-            {/* Category */}
-            <div className='space-y-1 text-sm'>
-              <label htmlFor='category' className='block text-gray-600 '>
-                Category
-              </label>
-              <select
-                required
-                className='w-full px-4 py-3 border-lime-300 focus:outline-lime-500 rounded-md bg-white'
-                name='category'
-              >
-                <option value='Indoor'>Indoor</option>
-                <option value='Outdoor'>Outdoor</option>
-                <option value='Succulent'>Succulent</option>
-                <option value='Flowering'>Flowering</option>
-              </select>
+              {errors.name && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.name.message}
+                </p>
+              )}
             </div>
             {/* Description */}
-            <div className='space-y-1 text-sm'>
-              <label htmlFor='description' className='block text-gray-600'>
+            <div className="space-y-1 text-sm">
+              <label htmlFor="description" className="block text-gray-600">
                 Description
               </label>
 
               <textarea
-                id='description'
-                placeholder='Write plant description here...'
-                className='block rounded-md focus:lime-300 w-full h-32 px-4 py-3 text-gray-800  border border-lime-300 bg-white focus:outline-lime-500 '
-                name='description'
+                id="description"
+                placeholder="Write plant description here..."
+                className="block rounded-md focus:lime-300 w-full h-32 px-4 py-3 text-gray-800  border border-lime-300 bg-white focus:outline-lime-500 "
+                name="description"
+                {...register("description", {
+                  required: "description is required",
+                })}
               ></textarea>
+              {errors.description && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.description.message}
+                </p>
+              )}
+            </div>
+            {/* Category */}
+            <div className="space-y-1 text-sm">
+              <label htmlFor="category" className="block text-gray-600 ">
+                Category
+              </label>
+              <select
+                className="w-full px-4 py-3 border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                {...register("category", { required: "category is required" })}
+              >
+                <option value="photography">photography</option>
+                <option value="sports">sports</option>
+                <option value="debate">debate</option>
+                <option value="cultural">cultural</option>
+                {errors.category && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.category.message}
+                  </p>
+                )}
+              </select>
+            </div>
+            {/* location */}
+            <div className="space-y-1 text-sm">
+              <label htmlFor="location" className="block text-gray-600 ">
+                location
+              </label>
+              <select
+                className="w-full px-4 py-3 border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                {...register("location", { required: "location is required" })}
+              >
+                <option value="bashabo">bashabo</option>
+                <option value="Outdoor">khilgao</option>
+                <option value="Succulent">motijheel</option>
+                <option value="Flowering">mirpur</option>
+                {errors.location && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.location.message}
+                  </p>
+                )}
+              </select>
             </div>
           </div>
-          <div className='space-y-6 flex flex-col'>
+          <div className="space-y-6 flex w-60 md:w-90 flex-col">
             {/* Price & Quantity */}
-            <div className='flex justify-between gap-2'>
+            <div className="flex justify-between gap-2">
               {/* Price */}
-              <div className='space-y-1 text-sm'>
-                <label htmlFor='price' className='block text-gray-600 '>
+              <div className="space-y-1 text-sm w-full">
+                <label htmlFor="price" className="block text-gray-600 ">
                   Price
                 </label>
                 <input
-                  className='w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
-                  name='price'
-                  id='price'
-                  type='number'
-                  placeholder='Price per unit'
-                  required
+                  className="w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                  id="price"
+                  type="number"
+                  placeholder="Price per unit"
+                  {...register("price", {
+                    required: "price is required",
+                    min: { value: 0, message: "price should more then 0" },
+                  })}
                 />
+                {errors.price && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.price.message}
+                  </p>
+                )}
               </div>
 
               {/* Quantity */}
-              <div className='space-y-1 text-sm'>
-                <label htmlFor='quantity' className='block text-gray-600'>
+              {/* <div className="space-y-1 text-sm">
+                <label htmlFor="quantity" className="block text-gray-600">
                   Quantity
                 </label>
                 <input
-                  className='w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
-                  name='quantity'
-                  id='quantity'
-                  type='number'
-                  placeholder='Available quantity'
-                  required
+                  className="w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                  id="quantity"
+                  type="number"
+                  placeholder="Available quantity"
+                  {...register("quantity", {
+                    required: "quantity is required",
+                    min: { value: 1, message: "price should more then 0" },
+                  })}
                 />
-              </div>
+                {errors.quantity && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.quantity.message}
+                  </p>
+                )}
+              </div> */}
+              {/* <div>hi</div> */}
             </div>
             {/* Image */}
-            <div className=' p-4  w-full  m-auto rounded-lg grow'>
-              <div className='file_upload px-5 py-3 relative border-4 border-dotted border-gray-300 rounded-lg'>
-                <div className='flex flex-col w-max mx-auto text-center'>
+            <div className=" p-4  w-full  m-auto rounded-lg grow">
+              <div className="file_upload px-5 py-3 relative border-4 border-dotted border-gray-300 rounded-lg">
+                <div className="flex flex-col w-max mx-auto text-center">
                   <label>
                     <input
-                      className='text-sm cursor-pointer w-36 hidden'
-                      type='file'
-                      name='image'
-                      id='image'
-                      accept='image/*'
+                      className="text-sm cursor-pointer w-36 hidden"
+                      type="file"
+                      id="image"
+                      accept="image/*"
                       hidden
+                      {...register("image", { required: "image is required" })}
                     />
-                    <div className='bg-lime-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-lime-500'>
+                    {errors.image && (
+                      <p className="text-red-500 text-xs mt-1">
+                        {errors.image.message}
+                      </p>
+                    )}
+                    <div className="bg-lime-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-lime-500">
                       Upload
                     </div>
                   </label>
@@ -104,16 +268,21 @@ const AddPlantForm = () => {
 
             {/* Submit Button */}
             <button
-              type='submit'
-              className='w-full cursor-pointer p-3 mt-5 text-center font-medium text-white transition duration-200 rounded shadow-md bg-lime-500 '
+              type="submit"
+              onSubmit={handleSubmit}
+              className="w-full cursor-pointer p-3 mt-5 text-center font-medium text-white transition duration-200 rounded shadow-md bg-lime-500 "
             >
-              Save & Continue
+              {isPending ? (
+                <TbFidgetSpinner className="animate-spin m-auto" />
+              ) : (
+                "Save & Continue"
+              )}
             </button>
           </div>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddPlantForm
+export default AddPlantForm;
